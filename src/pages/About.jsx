3318dd00f5fdf8803cb4a8bf381094adad6b4cd4
Sedetil <xtzysweet@gmail.com>
2023-12-03 08:11:57 +0700

    update about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 import { skills, experiences } from '../constants';
@@ -12,15 +13,15 @@ const About = () => {
       </h1>
 
       <div className="mt-5 flex flex-col gap-3 text-slate-500">
-        <p>Software Engineering based in Indonesia, specializing in technical education through hands-on learning and building application.</p>
+        <p>Software Engineer based in Indonesia, specializing in technical education through hands-on learning and building applications.</p>
       </div>
 
-      <div className="py-10 flex flex-col ">
+      <div className="py-10 flex flex-col">
         <h3 className="subhead-text">My Skills</h3>
 
         <div className="mt-16 flex flex-wrap gap-12">
-          {skills.map((skill) => (
-            <div className="block-container w-20 h-20 ">
+          {skills.map((skill, index) => (
+            <div key={`skill-${index}`} className="block-container w-20 h-20">
               <div className="btn-back rounded-xl" />
               <div className="btn-front rounded-xl flex justify-center items-center">
                 <img 
@@ -34,52 +35,53 @@ const About = () => {
         </div>
       </div>
 
-      <div className="py-16 ">
-         <h3 className="subhead-text">Work Experience</h3>
-          <div className="mt-5 flex flex-col gap-3 text-slate-500">
-          <p> I've worked with all sorts of companies, leveling up my skills and teaming up with smart people. Here's the rundown:</p>
+      <div className="py-16">
+        <h3 className="subhead-text">Work Experience</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>I've worked with various companies, honing my skills and collaborating with smart individuals. Here's the rundown:</p>
         </div>
 
         <div className="mt-12 flex">
-            <VerticalTimeline>
-              {experiences.map((experience) => (
-                <VerticalTimelineElement
-                  key={experience.company_name}
-                  date={experience.date}
-                  icon={<div className="flex justify-center items-center w-full h-full">
+          <VerticalTimeline>
+            {experiences.map((experience, index) => (
+              <VerticalTimelineElement
+                key={`experience-${index}`}
+                date={experience.date}
+                icon={
+                  <div className="flex justify-center items-center w-full h-full">
                     <img 
                       src={experience.icon}
                       alt={experience.company_name}
                       className="w-[60%] h-[60%] object-contain"
                     />
-                  </div>}
-                  iconStyle={{ background: experience.iconBg }}
-                  contentStyle={{
-                    borderBottom: '8px',
-                    borderStyle: 'solid',
-                    borderBottomColor: experience.iconBg,
-                    boxShadow: 'none',
-                  }}
-                >
-                  <div>
-                    <h3 className="text-black text-xl font-poppins font-semibold">
-                      {experience.title}
-                    </h3>
-                    <p className="text-black-500 font-mediumfont-base" style={{margin:0}}>
-                      {experience.company_name}
-                    </p>
                   </div>
+                }
+                iconStyle={{ background: experience.iconBg }}
+                contentStyle={{
+                  borderBottom: '8px solid',
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: 'none',
+                }}
+              >
+                <div>
+                  <h3 className="text-black text-xl font-poppins font-semibold">
+                    {experience.title}
+                  </h3>
+                  <p className="text-black-500 font-medium font-base" style={{ margin: 0 }}>
+                    {experience.company_name}
+                  </p>
+                </div>
 
-                  <ul className="my-5 list-disc ml-5 space-y-2">
-                    {experience.points.map((point, index) => (
-                      <li key={`experience-poin-${index}`} className="text-black-500/50 font-normal pl-1 text-sm">
-                        {point}
-                      </li>
-                    ))}
-                  </ul>
-                </VerticalTimelineElement>
-              ))}
-            </VerticalTimeline>
+                <ul className="my-5 list-disc ml-5 space-y-2">
+                  {experience.points.map((point, pointIndex) => (
+                    <li key={`experience-point-${pointIndex}`} className="text-black-500/50 font-normal pl-1 text-sm">
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
         </div>
       </div>
 
@@ -87,7 +89,7 @@ const About = () => {
 
       <CTA />
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
