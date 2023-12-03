be6219766dbc0ebdfffc008965123658ba038b04
Sedetil <115119502+Sedetil@users.noreply.github.com>
2023-12-03 08:53:17 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Island from '../models/Island';
 import Sky from '../models/Sky';
 import Bird from '../models/Bird';
 import Plane from '../models/Plane';
-import HomeInfo from '../components/Homeinfo';
+import Homeinfo from '../components/HomeInfo';
 
 import sakura from '../assets/sakura.mp3';
 import { soundoff, soundon } from '../assets/icons';
