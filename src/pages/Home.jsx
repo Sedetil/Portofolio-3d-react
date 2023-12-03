532ffb797e4b4bdd4470f1675d881b74ac3675b6
Sedetil <115119502+Sedetil@users.noreply.github.com>
2023-12-03 08:46:01 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import { useState, Suspense, useEffect, useRef } from 'react'
-import { Canvas } from '@react-three/fiber'
-import Loader from '../components/Loader'
+import { useState, Suspense, useEffect, useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import Loader from '../components/Loader';
 
 import Island from '../models/Island';
 import Sky from '../models/Sky';
@@ -8,7 +8,7 @@ import Bird from '../models/Bird';
 import Plane from '../models/Plane';
 import Homeinfo from '../components/Homeinfo';
 
-import sakura from '../assets/sakura.mp3'
+import sakura from '../assets/sakura.mp3';
 import { soundoff, soundon } from '../assets/icons';
 
 const Home = () => {
@@ -16,59 +16,59 @@ const Home = () => {
   audioRef.current.volume = 0.4;
   audioRef.current.loop = true;
   const [isRotating, setIsRotating] = useState(false);
-  const [currentStage, setCurrentStage] = useState(1)
+  const [currentStage, setCurrentStage] = useState(1);
   const [isPlayingMusic, setIsPlayingMusic] = useState(false);
 
-  useEffect (() => {
-    if(isPlayingMusic) {
+  useEffect(() => {
+    if (isPlayingMusic) {
       audioRef.current.play();
     }
 
     return () => {
       audioRef.current.pause();
-    }
-  }, [isPlayingMusic])
+    };
+  }, [isPlayingMusic]);
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
     let rotation = [0.1, 4.7, 0];
 
-    if(window.innerWidth < 768) {
+    if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
     } else {
       screenScale = [1, 1, 1];
     }
 
     return [screenScale, screenPosition, rotation];
-  }
+  };
 
   const adjustPlaneForScreenSize = () => {
     let screenScale, screenPosition;
 
-    if(window.innerWidth < 768) {
+    if (window.innerWidth < 768) {
       screenScale = [1.5, 1.5, 1.5];
-      screenPosition = [0, -1.5, 0]
+      screenPosition = [0, -1.5, 0];
     } else {
       screenScale = [3, 3, 3];
-      screenPosition = [0, -4, -4]
+      screenPosition = [0, -4, -4];
     }
 
     return [screenScale, screenPosition];
-  }
+  };
 
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
-  const [planeScale, planePosition ] = adjustPlaneForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
-    <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-      {currentStage && <Homeinfo currentStage={currentStage} />}
-    </div>
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <Homeinfo currentStage={currentStage} />}
+      </div>
 
-      <Canvas 
+      <Canvas
         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
-        camera={{ near: 0.1, for: 1000 }}
+        camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={2} />
@@ -86,7 +86,7 @@ const Home = () => {
             setCurrentStage={setCurrentStage}
           />
           <Plane
-            isRotating={isRotating} 
+            isRotating={isRotating}
             scale={planeScale}
             position={planePosition}
             rotation={[0, 20, 0]}
@@ -103,7 +103,7 @@ const Home = () => {
         />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
